625b406f1a78ee7f808c00177428808a610e411c
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-05-12 16:59:40 -0400

    finish graph info

--- graph.py
+++ graph.py
@@ -7,6 +7,7 @@ class Graph:
         self.edges = edges
 
     def graph_info(self, node_style="None", edge_style="None", output_style="None"):
+        # form the representation
         if node_style == "None":
             all_node = [each_node.value for each_node in self.nodes]
         elif node_style == "Full":
@@ -17,5 +18,28 @@ class Graph:
         if edge_style == "Full":
             all_edge = [(each_edge.node1, each_edge.node2, each_edge.weight, each_edge.capacity) for each_edge in self.edges]
 
-        return (all_node, all_edge)
+        # format the output
+        if output_style == "None":
+            return (all_node, all_edge)
+        elif output_style == "dict":
+            output = {
+                "nodes" : all_node,
+                "edges" : {}
+            }
+
+            for each_edge_index in range(len(self.edges)):
+                if edge_style == "None":
+                    output["edges"][str(each_edge_index)] = {
+                        "node1" : self.edges[each_edge_index].node1,
+                        "node2" : self.edges[each_edge_index].node2
+                    }
+                elif edge_style == "Full":
+                    output["edges"][str(each_edge_index)] = {
+                        "node1" : self.edges[each_edge_index].node1,
+                        "node2" : self.edges[each_edge_index].node2,
+                        "weight" : self.edges[each_edge_index].weight,
+                        "capacity" : self.edges[each_edge_index].capacity
+                    }
+
+        return output
         
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from graph import Graph
 import numpy as np
 
 test_node = [Node(i, np.random.rand()) for i in range(10)]
-test_edge = [Edge(np.random.randint(1, 11), np.random.randint(1, 11), np.random.rand(), np.random.rand) for i in range(100) if np.random.rand() > 0.3]
+test_edge = [Edge(np.random.randint(1, 11), np.random.randint(1, 11), np.random.random(), np.random.random()) for i in range(100) if np.random.rand() > 0.3]
 
 test_graph = Graph(test_node, test_edge)
 
-print(test_graph.graph_info())
+print(test_graph.graph_info(node_style="Full", edge_style="None", output_style="dict"))
